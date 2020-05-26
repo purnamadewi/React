614ee56cbc5157ef7bcd0f19d806a44962d11bf9
purnamadewi <61812709+purnamadewi@users.noreply.github.com>
2020-05-26 23:50:59 +0800

    Setup Part 1 & Part 2
    Setup Part 1 dan Part 2 React pemrograman visual

---
+++ Setup Part 1 dan Part 2/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SideBar from './components/Sidebar';
+import CreateAppointment from './components/appointment/Create';
+import AppointmentList from './components/appointment/List';
+import TodayAppointment from './components/appointment/Today';
+
+function App() {
+  return (
+    <div>
+      <SideBar/>
+      <CreateAppointment/>
+      <AppointmentList/>
+      <TodayAppointment/>
+    </div>
+  );
+}
+
+export default App;
